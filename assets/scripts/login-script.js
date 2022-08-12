cdddfde029bc779cdb659c3dd4266b0f2aefe8b8
RipanRenaldi <84433321+RipanRenaldi@users.noreply.github.com>
2022-08-12 22:11:59 +0700

    Update login-script.js
    Menambah beberapa komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,18 +5,23 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+// Menangkap seluruh element input form
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// create a variabel that store username and password should be
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+
+// giving an event into login form element when the form is submited
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+  // Validate email and password user inputed with email and password expected
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
